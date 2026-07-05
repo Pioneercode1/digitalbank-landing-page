8a1df2de7128a064943189c73458e3e63a563878
Pioneercode <pioneercode@outlook.com>
2026-07-05 11:30:40 +0300

    commet 2

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const btnMenu = document.querySelector('.btn-menu');
+const iconMenu = document.querySelector('.icon-menu');
+const menu = document.querySelector('.menu');
+const imgShowMenu = document.querySelector('.show-menu');
+
+btnMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+    if (menu.classList.contains('active')) {
+        iconMenu.src = "./images/icon-close.svg";
+        imgShowMenu.classList.toggle('show-menu');
+    } else {
+        iconMenu.src = "./images/icon-hamburger.svg";
+        imgShowMenu.classList.toggle('show-menu');
+    }
+});
